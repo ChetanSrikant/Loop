c675d615cc9c6a435b08dbd7dc816b658979ff48
ChetanSrikant <chetansmandiga@gmail.com>
2024-10-16 23:58:59 +0530

    setup profile page

--- Loop/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ Loop/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
+import { useState } from 'react'
 
 const ProfileUpdate = () => {
+
+  const [image, setImage] = useState(false)
+
   return (
-    <div>
-      We are on the Profile Page
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Detailes</h3>
+          <label htmlFor="avatar">
+            <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={image? URL.createObjectURL(image) : assets.avatar_icon}/>
+            upload profile image
+          </label>
+          <input type="text" placeholder='Your name' required/>
+          <textarea placeholder='Write profile bio' required>
+          </textarea>
+          <button type='submit'>Save</button> 
+        </form>
+        <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon}/>
+      </div>
     </div>
   )
 }
